871447365c413fe5295629c99e68d805531c72ae
dudeSafiyur1234 <62690183+dudeSafiyur1234@users.noreply.github.com>
2020-06-29 06:19:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ const config = require("./config.json");
 const discord = require("discord.js");
 const client = new discord.Client();
 const Trello = require("trello");
-const trello = new Trello(config.trelloAppKey,config.trelloToken);
+const trello = new Trello(config.trelloKey, config.trelloToken);
 
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.on("ready", () => {
+  console.log("I am ready!");
+  client.user.setPresence({ activity: { name: 'trello bans' }, status: 'online' })
 });
 
 client.on("message", message => {
   if (message.author.bot) return;
   // This is where we'll put our code.
   if (message.content.indexOf(config.prefix) !== 0) return;
- 
+
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
- 
+
   if(command === 'help') {
     message.channel.send({embed: {
     color: 3447003,
@@ -28,58 +28,73 @@ client.on("message", message => {
     description: "Hey! This is a bot for tbans! Any support or questions? Use the support link below!",
     fields: [{
         name: "Prefix",
-        value: `The current prefix is ${config.prefix}.`
+        value: `The current prefix is ${config.prefix}. `
       },
       {
         name: "Commands.",
-        value: "The current commands are: `help` and `tban`."
+        value: "The current commands are: `help`,`tban`, and `misc`"
       },
       {
         name: "Support server.",
-        value: "Here is the link for the support server: https://discord.gg/3GaYG48."
+        value: "Here is the link for the support server of the original bot scripter: https://discord.gg/3GaYG48."
       }
     ],
     timestamp: new Date(),
     footer: {
       icon_url: client.user.avatarURL,
-      text: "Trello ban bot"
+      text: "Trello Bans"
     }
   }
 });
   } else
   if (command === 'tban') {
-    if(!message.member.roles.some(r=>["TBan Permission"].includes(r.name)) )
+    if(!message.member.roles.cache.some(role => role.name === 'TBan Permission')) 
             return message.channel.send({embed: {
                 color: 15406156,
-                description: "You do not have permission to run this command.",
+                description: "You do not have permission to use this command.",
                 author: {
                     name: message.author.tag,
                     icon_url: message.author.displayAvatarURL
                 }
             }});
-   let targetUser = args[0];
-    if (!targetUser) return  message.channel.send({embed: {
-  color: 3447003,
-  description: "Too few arguments given. `!tban [username:userid][reason]` **REMEMBER THE COLON BETWEEN THE USERNAME AND USERID**"
-}});
-   let banReason = args.slice(1).join(" ");
-    if (!banReason) return  message.channel.send({embed: {
-  color: 3447003,
-  description: "Please put in a reason. If there isn't one, put a symbol to indicate this, like a dash(-)."
-}});
-   let listID = config.listID;
-  trello.addCard(targetUser, banReason, listID,
+    let suspect = args[0]
+    if (!suspect) {
+       return message.channel.send({embed: {
+                color: 15406156,
+                description: "Use this format: `!tban [Username:UserID][banReason]`",
+                author: {
+                    name: message.author.tag,
+                    icon_url: message.author.displayAvatarURL
+                }
+            }}); 
+    };
+    let reason = args.slice(1).join(" ");
+    if (!reason) {
+       return message.channel.send({embed: {
+                color: 15406156,
+                description: "Use this format: `!tban [Username:UserID][banReason]`",
+                author: {
+                    name: message.author.tag,
+                    icon_url: message.author.displayAvatarURL
+                }
+            }}); 
+    };
+      trello.addCard(suspect, reason, config.listID,
       function (error, trelloCard) {
           if (error) {
-              console.log('An error occured with Trello card.' + error)
-            message.channel.send({embed: {
-  color: 3447003,
-  description: "An error occured! Please try again later or use the support server link."
-}});
+              console.log("An error occured", error)
+               return message.channel.send({embed: {
+                color: 15406156,
+                description: "An error occured. Please try again later. \n ERROR:" + error,
+                author: {
+                    name: message.author.tag,
+                    icon_url: message.author.displayAvatarURL
+                }
+            }}); 
           }
           else {
               console.log('Added card:', trelloCard);
-            message.channel.send({embed: {
+              return message.channel.send({embed: {
     color: 3447003,
     author: {
       name: client.user.username,
@@ -89,23 +104,26 @@ client.on("message", message => {
     description: "Card details:",
     fields: [{
         name: "Suspect:",
-        value: targetUser
+        value: suspect,
       },
       {
         name: "Ban Reason",
-        value: banReason
+        value: reason,
       },
     ],
     timestamp: new Date(),
     footer: {
       icon_url: client.user.avatarURL,
-      text: "Trello ban"
+      text: "Trello Bans."
     }
   }
 });
           }
       });
-  }
+  }else;
+  if (command === 'misc') {
+     message.channel.send("My uptime is: `" + client.uptime + "ms` \nMy ping is: `" + client.ws.ping + "ms`");
+   }
 });
 
-client.login(config.token);
+client.login(config.token)
